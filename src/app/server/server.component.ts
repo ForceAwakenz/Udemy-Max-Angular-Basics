b69f4b3f919e1ff808e12fe65c5a4d939ec1cc89
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-20 11:10:42 +0300

    add ngStyle and some styling logic

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: ['.online {color: green;} .offline {color: salmon;}']
 })
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string;
+  fetching = 'fetching...';
+  loading = 'loading...';
+  color = '';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    setTimeout( () => {
+      this.fetching = 'done';
+      this.loading = 'data is successfully loaded';
+      this.color = 'lightgreen';
+    }, 4500);
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
